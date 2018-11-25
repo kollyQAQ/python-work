411337b46e1ac8fe188019da0b2ebbe87d3f3e0a
xinxing.duan <xinxing.duan@jyblife.com>
2018-11-25 17:55:52 +0800

    123

--- itchatTest/main.py
+++ itchatTest/main.py
@@ -2,6 +2,7 @@ import re
 import os
 import itchat
 from itchat.content import *
+from itchatTest.utils import contact
 import random
 from time import sleep
 from itchatTest.scheduler import send_weather_info #勿删
@@ -11,7 +12,7 @@ CMD_CHAT = 'chat'
 CMD_SHUTUP = 'shutup'
 CMD_SHOW = 'show'
 
-friendsName = ''
+friendsName = contact.get_user_name_by_remark('赖皮糖')
 chatRoomName = ''
 chatRoomUserList = []
 chatRoomUserAll = False
@@ -20,8 +21,10 @@ chatPrefix = ''
 # 收到加好友邀请
 @itchat.msg_register(FRIENDS)
 def add_friend(msg):
+    print(msg)
     msg.user.verify()
-    msg.user.send('Nice to meet you!  I\'m a robot ~')
+    msg.user.send('终于等到你啦，主人~')
+    msg.user.send('我是你的贴心机器人，可以陪你聊天，给你唱歌、讲段子，还有好玩的小游戏哦')
 
 # 收到好友文字消息
 @itchat.msg_register(TEXT, isFriendChat=True)
@@ -33,16 +36,17 @@ def text_reply(msg):
     print('收到来自【'+friendsName+'】【' + msg['User'].NickName + '】的消息：' + msg['Text'])
 
     itchat.send_msg(msg['Text'], iceName)
+
     return None
 
 # 收到好友图片/表情消息
 @itchat.msg_register(PICTURE, isFriendChat=True)
-def contents_reply(msg):
+def picture_reply(msg):
     global friendsName, iceName
 
     friendsName = msg['FromUserName']
     fileName = msg['FileName']
-    print('收到来自【' + friendsName + '】的图片' + fileName)
+    print('收到来自【' + friendsName + '】的图片:' + fileName)
 
     # 下载图片
     msg['Text'](fileName)
@@ -51,7 +55,21 @@ def contents_reply(msg):
     # 删除图片
     os.remove(fileName)
 
-# 收到公众号消息
+# 收到好友语音消息
+@itchat.msg_register(RECORDING, isFriendChat=True)
+def picture_reply(msg):
+    global friendsName, iceName
+
+    friendsName = msg['FromUserName']
+    fileName = msg['FileName']
+    print('收到来自【' + friendsName + '】的语音:' + fileName)
+
+    # 下载语音
+    msg['Text']('D:\workspace-demo\python-work\itchatTest\static\other\\' + fileName)
+    sleep(5)
+    itchat.send_msg('等我升级后再跟你说话~', friendsName)
+
+# 收到小冰的文本消息
 @itchat.msg_register(TEXT, isMpChat=True)
 def text_reply(msg):
     global friendsName, iceName
@@ -66,9 +84,9 @@ def text_reply(msg):
         # friendsName = ''
     return None
 
-# 收到公号的的图片/表情
+# 收到小冰的的图片/表情
 @itchat.msg_register(PICTURE, isMpChat=True)
-def contents_reply(msg):
+def picture_reply(msg):
     global friendsName, iceName
 
     if msg['FromUserName'] == iceName and friendsName != '':
@@ -84,85 +102,101 @@ def contents_reply(msg):
         os.remove(fileName)
     return None
 
-# 收到群聊的消息
-@itchat.msg_register(TEXT, isGroupChat=True)
-def text_reply(msg):
-    global chatRoomName, chatRoomUserList, chatRoomUserAll, chatPrefix, iceName
+# 收到小冰的语音消息
+@itchat.msg_register(RECORDING, isMpChat=True)
+def recording_reply(msg):
+    global friendsName, iceName
 
-    fromText = msg['Text']
-    fromUserName = msg['FromUserName']
-    actualNickName = msg['ActualNickName']
-    chatRoomName = msg['User'].NickName
-    try:
-        if fromText[0] == '#':
-            if chatRoomName != '' and chatRoomName != msg['FromUserName']:
-                itchat.send_msg('sorry,msice is busy~', msg['FromUserName'])
-            list = re.split('\s+', fromText[1:], 2)
-            if '' in list:
-                list.remove('')
-            cmd = list[0].strip().lower()
-            if cmd == CMD_CHAT:
-                user = actualNickName if len(list) < 2 else list[1]
-                if user.lower() == 'all':
-                    chatRoomUserAll = True
-                    chatRoomUserList = []
-                else:
-                    if user in chatRoomUserList:
-                        raise Exception('%s is already in chatlist' % user)
-                    chatRoomUserList.append(user)
-                chatRoomName = fromUserName
-            elif cmd == CMD_PREFIX:
-                if len(list) < 2:
-                    chatPrefix = ''
-                else:
-                    if chatPrefix == '#':
-                        raise Exception('prefix can not be #')
-                    else:
-                        chatPrefix = list[1]
-            elif cmd == CMD_SHUTUP:
-                user = msg['ActualNickName'] if len(list) < 2 else list[1]
-                if user.lower() == 'all':
-                    chatRoomUserAll = False
-                    chatRoomUserList = []
-                else:
-                    if user in chatRoomUserList:
-                        chatRoomUserList.remove(user)
-                    else:
-                        raise Exception('%s is not in chatlist at all' % user)
-            elif cmd == CMD_SHOW:
-                itchat.send_msg('list : %s\nprefix : %s' %
-                                    (chatRoomUserList, chatPrefix), fromUserName)
-            else:
-                raise Exception('command not found')
-            itchat.send_msg('cmd success \nlist : %s\nprefix : %s' %
-                                (chatRoomUserList, chatPrefix), fromUserName)
-        elif fromText[:].startswith(chatPrefix):
-            print('收到来自群聊【' + chatRoomName + '】【' + actualNickName + '】的消息:' + fromText)
-            if chatRoomUserAll or actualNickName in chatRoomUserList:
-                itchat.send_msg(123456, fromUserName)
-    except Exception as e:
-        itchat.send_msg(e.args[0], fromUserName)
+    if msg['FromUserName'] == iceName and friendsName != '':
+        fileName = msg['FileName']
+        print('收到来自小冰的语音:' + fileName)
+        # 下载语音
+        msg['Text']('D:\workspace-demo\python-work\itchatTest\static\ice\\' + fileName)
+        # 转发语音给好友
+        sleep(5)
+        itchat.send_file(fileName, friendsName)
+        # friendsName = ''
     return None
 
-# 收到群聊的图片/表情
-@itchat.msg_register(PICTURE, isGroupChat=True)
-def contents_reply(msg):
-    fileName = msg['FileName']
-    fromUserName = msg['FromUserName']
-    actualNickName = msg['ActualNickName']
-    chatRoomName = msg['User'].NickName
-    try:
-        print('收到来自群聊【' + chatRoomName + '】【' + actualNickName + '】的图片:' + fileName)
-        if chatRoomName == '微信测试群' and actualNickName.startswith('kolly'):
-            itchat.send_msg(123456, fromUserName)
-    except Exception as e:
-        itchat.send_msg(e.args[0], fromUserName)
-    return None
+# # 收到群聊的消息
+# @itchat.msg_register(TEXT, isGroupChat=True)
+# def text_reply(msg):
+#     global chatRoomName, chatRoomUserList, chatRoomUserAll, chatPrefix, iceName
+#
+#     fromText = msg['Text']
+#     fromUserName = msg['FromUserName']
+#     actualNickName = msg['ActualNickName']
+#     chatRoomName = msg['User'].NickName
+#     try:
+#         if fromText[0] == '#':
+#             if chatRoomName != '' and chatRoomName != msg['FromUserName']:
+#                 itchat.send_msg('sorry,msice is busy~', msg['FromUserName'])
+#             list = re.split('\s+', fromText[1:], 2)
+#             if '' in list:
+#                 list.remove('')
+#             cmd = list[0].strip().lower()
+#             if cmd == CMD_CHAT:
+#                 user = actualNickName if len(list) < 2 else list[1]
+#                 if user.lower() == 'all':
+#                     chatRoomUserAll = True
+#                     chatRoomUserList = []
+#                 else:
+#                     if user in chatRoomUserList:
+#                         raise Exception('%s is already in chatlist' % user)
+#                     chatRoomUserList.append(user)
+#                 chatRoomName = fromUserName
+#             elif cmd == CMD_PREFIX:
+#                 if len(list) < 2:
+#                     chatPrefix = ''
+#                 else:
+#                     if chatPrefix == '#':
+#                         raise Exception('prefix can not be #')
+#                     else:
+#                         chatPrefix = list[1]
+#             elif cmd == CMD_SHUTUP:
+#                 user = msg['ActualNickName'] if len(list) < 2 else list[1]
+#                 if user.lower() == 'all':
+#                     chatRoomUserAll = False
+#                     chatRoomUserList = []
+#                 else:
+#                     if user in chatRoomUserList:
+#                         chatRoomUserList.remove(user)
+#                     else:
+#                         raise Exception('%s is not in chatlist at all' % user)
+#             elif cmd == CMD_SHOW:
+#                 itchat.send_msg('list : %s\nprefix : %s' %
+#                                     (chatRoomUserList, chatPrefix), fromUserName)
+#             else:
+#                 raise Exception('command not found')
+#             itchat.send_msg('cmd success \nlist : %s\nprefix : %s' %
+#                                 (chatRoomUserList, chatPrefix), fromUserName)
+#         elif fromText[:].startswith(chatPrefix):
+#             print('收到来自群聊【' + chatRoomName + '】【' + actualNickName + '】的消息:' + fromText)
+#             if chatRoomUserAll or actualNickName in chatRoomUserList:
+#                 itchat.send_msg(123456, fromUserName)
+#     except Exception as e:
+#         itchat.send_msg(e.args[0], fromUserName)
+#     return None
+#
+# # 收到群聊的图片/表情
+# @itchat.msg_register(PICTURE, isGroupChat=True)
+# def picture_reply(msg):
+#     fileName = msg['FileName']
+#     fromUserName = msg['FromUserName']
+#     actualNickName = msg['ActualNickName']
+#     chatRoomName = msg['User'].NickName
+#     try:
+#         print('收到来自群聊【' + chatRoomName + '】【' + actualNickName + '】的图片:' + fileName)
+#         if chatRoomName == '微信测试群' and actualNickName.startswith('kolly'):
+#             itchat.send_msg(123456, fromUserName)
+#     except Exception as e:
+#         itchat.send_msg(e.args[0], fromUserName)
+#     return None
 
 # 微信登录
 # itchat.auto_login(enableCmdQR=2, hotReload=True)
-# itchat.auto_login(hotReload=True)
-itchat.auto_login()
+itchat.auto_login(hotReload=True)
+# itchat.auto_login()
 
 # 查找小冰公众号名称保存到 iceName 变量
 ice = itchat.search_mps(name='小冰')
--- itchatTest/scheduler/send_weather_info.py
+++ itchatTest/scheduler/send_weather_info.py
@@ -5,7 +5,10 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 # 发送天气信息
 def send_weather_info():
-    itchat.send(weather_util.get_weather_today(), contact.get_user_name_by_remark('kolly'))
+    # itchat.send(weather_util.get_weather_today(), contact.get_user_name_by_remark('kolly'))
+    ice = itchat.search_mps(name='小冰')
+    iceName = ice[0]['UserName']
+    itchat.send('深圳天气', iceName)
 
 # 发送笑话
 def send_joke():
@@ -13,7 +16,7 @@ def send_joke():
     iceName = ice[0]['UserName']
     itchat.send('讲个笑话', iceName)
 
-    # 发送笑话
+# 发送八卦娱乐新闻
 def send_news():
     ice = itchat.search_mps(name='小冰')
     iceName = ice[0]['UserName']
@@ -21,7 +24,7 @@ def send_news():
 
 # BackgroundScheduler
 scheduler = BackgroundScheduler()
-scheduler.add_job(send_weather_info, 'cron', day_of_week='mon-sun', hour=7, minute=20) #corn表达式
-scheduler.add_job(send_joke, 'cron', day_of_week='mon-sun', hour=7, minute=21) #corn表达式
-scheduler.add_job(send_news, 'cron', day_of_week='mon-sun', hour=7, minute=22) #corn表达式
+scheduler.add_job(send_weather_info, 'cron', day_of_week='mon-sun', hour=7, minute=0) #corn表达式
+scheduler.add_job(send_joke, 'cron', day_of_week='mon-sun', hour=7, minute=15) #corn表达式
+scheduler.add_job(send_news, 'cron', day_of_week='mon-sun', hour=12, minute=30) #corn表达式
 scheduler.start()
